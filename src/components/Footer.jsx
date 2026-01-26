2770b334751513a6767ac13aeb28e36084624550
Abdelrahman Ashraf <abdelrahmanashraf6000@gmail.com>
2026-01-26 22:55:08 +0200

    update: added my new portfolio site

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
     return <footer className="py-4 bg-gray-900/70 text-white px-5 sm:px-10 border-t-2 border-gray-600">
         <div className="mx-auto text-center">
-            Copyright &copy; {new Date().getFullYear()} designed by <span className="text-red-500 font-semibold"><a href="https://github.com/AbdelrahmanAshraf62651" target="_blank">3ATEF</a></span>
+            Copyright &copy; {new Date().getFullYear()} designed by <span className="text-red-500 font-semibold"><a href="https://portfolio-frontend-lemon-gamma.vercel.app/" target="_blank">3ATEF</a></span>
         </div>
     </footer>
 }
